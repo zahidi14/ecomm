9171488616d1b242916fe2669c2a07a89c41197d
zahidi14 <d.zahidi1412@gmail.com>
2024-07-09 18:08:01 +0800

    first

---
+++ src/app/auth/layout.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Authlayout({ children }) {
+  return (
+    <div>
+      <h1>Register</h1>
+      <div>{children}</div>
+    </div>
+  );
+}
---
+++ src/app/auth/signin/page.js
@@ -0,0 +1,79 @@
+"use client";
+
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { auth } from "../../../../firebaseConfig";
+export default function test() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      console.log("yeeeym,90");
+      router.push("/");
+    } catch (err) {
+      console.log("error", err);
+    }
+  };
+  return (
+    <div className="w-full max-w-xs mx-auto">
+      <form
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        onSubmit={handleSubmit}
+      >
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="email"
+          >
+            Email
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="email"
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div className="mb-6">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="password"
+          >
+            Password
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="password"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Sign In
+          </button>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            onClick={() => router.push("/")}
+          >
+            test
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
